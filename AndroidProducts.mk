a03b32c18d9b634b3ff0ace5bc987a0713d6f327
Romlord14495 <eddiebob146@gmail.com>
2026-08-25 02:09:49 -0500

    Update AndroidProducts.mk
    Added OrangeFox

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,11 @@
-
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_nevada.mk
+    $(LOCAL_DIR)/twrp_nevada.mk \
+    $(LOCAL_DIR)/omni_nevada.mk
 
 COMMON_LUNCH_CHOICES := \
     twrp_nevada-user \
     twrp_nevada-userdebug \
-    twrp_nevada-eng
+    twrp_nevada-eng \
+    omni_nevada-user \
+    omni_nevada-userdebug \
+    omni_nevada-eng
